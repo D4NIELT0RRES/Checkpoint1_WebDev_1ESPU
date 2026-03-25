c8704092cadf551902cb55abd14ad296c7647ed0
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 19:34:05 -0300

    Questão 4, desenvolvida com sucesso, o objetivo é criar algumas variáveis no arquivo script.js e atribuir um valor a ela

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*==========================================================================================
+/*================================== QUESTÃO 3 ============================================
 #  OBJETIVO: Repositório para o Checkpoint #1 de Engenharia de Software 
 #  DATA: 25/03/2025
 #  AUTORES: DANIEL GOMES TORRES - GUILHERME DE CORREIA ALVES - PEDRO PASSOS CORSINI
@@ -22,5 +22,29 @@ console.log("Mensagem inicial")
 alert("Bem-vindo ao Checkpoint!")
 
 
+/*==================================================================================*/
+/******* QUESTÃO - 04 *******/
+
+// Variáveis com var
+var nome = "Guilherme";
+var idade = 20;
+
+// Variáveis com let
+let cidade = "São Paulo";
+let curso = "Engenharia de Software";
+
+// Variáveis com const
+const pais = "Brasil";
+const ano = 2026;
+
+// Exibindo no console
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+
+console.log("Cidade:", cidade);
+console.log("Curso:", curso);
+
+console.log("País:", pais);
+console.log("Ano:", ano);
 
 /*==================================================================================*/
